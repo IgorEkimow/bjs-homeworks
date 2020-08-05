4087c0c09d42e90c0b3b5ef0498b774ec199b74c
Igor <igorekimow@gmail.com>
2020-08-05 10:26:22 +0300

    --no-edit

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,50 +1,38 @@
-function getResult(a,b,c){
-    "use strict"
+"use strict";
 
-    let D = Math.pow(b, 2) - 4 * a * c; // дискрименант
+function getResult(a,b,c){
+    let D = Math.pow(b, 2) - 4 * a * c;
     let x = [];
 
-    if(D < 0) {
-        return x;
-    } else if (D === 0) {
+    if (D === 0) {
         x[0] = -b / 2 * a;
-        return x;
     } else if (D > 0) {
         x[0] = (-b + Math.sqrt(D)) / (2 * a);
         x[1] = (-b - Math.sqrt(D)) / (2 * a);
-        return x;
     }
+    return x;
 }
 
 function getAverageMark(marks){
-    if(marks.length > 0) {
-        if(marks.length > 5) {
-            console.log('Количество оценок больше 5');
-            marks = marks.slice(0, 5);
-        }
-        let averageMark = 0;
-        for (let i = 0; i < marks.length; i++) {
-            averageMark += marks[i] / marks.length;
-        }
-        return averageMark;
-    } else {
+    if(marks.length === 0) {
         return 0;
     }
+    if(marks.length > 5) {
+        console.log('Количество оценок больше 5');
+        marks = marks.slice(0, 5);
+    }
+    let averageMark = 0;
+    for (let i = 0; i < marks.length; i++) {
+        averageMark += marks[i] / marks.length;
+    }
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    let todayDate = new Date();
-    todayDate = todayDate.getFullYear();
-    let userDate = new Date(dateOfBirthday);
-    userDate = userDate.getFullYear();
-
-    let finalDate = todayDate - userDate;
-
+    let finalDate = new Date().getFullYear() - dateOfBirthday.getFullYear();
     if(finalDate > 18) {
-        let result = `Не желаете ли олд-фэшн, ${name}?`;
-        return result;
+        return `Не желаете ли олд-фэшн, ${name}?`;
     } else {
-        let result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
-        return result;
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
     }
 }
