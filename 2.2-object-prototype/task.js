1bd5286010e1d4059f224596ad7f8c7cf667fe4f
Igor <igorekimow@gmail.com>
2020-08-30 13:46:17 +0300

    Done

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,32 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function() {
+    let str = this.toLowerCase().replace(/[^а-яa-z1-9]/gi, '');
+    const lastIndex = str.length - 1;
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[lastIndex - i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum += Number(marks[i]);
+    }
+    const average = sum/marks.length;
+    const roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const now  = new Date();
+    const birth = new Date(birthday);
+    const diff = now - birth;
+    const age = diff / (1000 * 60 * 60 * 24 * 365.242);
+    const result = age > 18 ? true : false;
+    return result;
 }
